74923ad22303925c7ecb786d55ec8c34d5104b46
mennanasa12-cloud <mennanasa12@gmail.com>
2026-08-27 00:12:41 +0300

    Add agent/decision_agent.py

---
+++ agent/decision_agent.py
@@ -0,0 +1,11 @@
+"""
+Given ML prediction + DL anomaly flag + RAG context, decide:
+- inform engineer
+- generate report
+- run additional check
+- do nothing
+"""
+
+def decide(ml_prediction, dl_flag, rag_context):
+    # TODO: call LLM with tool-calling for the decision
+    pass
